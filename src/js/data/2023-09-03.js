73149ef2eac46bbc4637fa61cd0141363daa4aaa
ma-che-marie <143934357+ma-che-marie@users.noreply.github.com>
2023-09-03 17:34:14 -0700

    Update 2023-09-03.js

--- src/js/data/2023-09-03.js
+++ src/js/data/2023-09-03.js
@@ -14,7 +14,32 @@ dataSet[dataSetVersion].options = [
       { name: "Season 4 Finalists", key: "S4" },
       { name: "Season 4 Contestants (Non-Finalists)", key: "S4all" }
     ]
-  }
+  },
+	{
+    name: "Filter by Voice Type",
+    key: "type",
+    tooltip: "Check this to restrict to singers of a certain voice type/music background.",
+    checked: false,
+    sub: [ 
+      { name: "Classical (성악가)", key: "classical" }, 
+      { name: "Musical actor (뮤지컬배우)", key: "musical" }, 
+      { name: "Sorikkun (소리꾼)", key: "gugak" }, 
+      { name: "Rock/Pop (로커/팝 보컬)", key: "rockpop" }
+    ]
+  },
+{
+    name: "Filter by Vocal Range",
+    key: "range",
+    tooltip: "Check this to restrict to singers of a certain vocal range.",
+    checked: false,
+    sub: [ 
+      { name: "Bass (베이스)", key: "bass" }, 
+      { name: "Bass Baritone (베이스 바리톤)", key: "bb" }, 
+      { name: "Baritone (바리톤)", key: "baritone" }, 
+      { name: "Tenor (테너)", key: "tenor" }, 
+      { name: "Countertenor (카운터테너)", key: "counter" }
+    ]
+  },
 ];
 
 dataSet[dataSetVersion].characterData = [

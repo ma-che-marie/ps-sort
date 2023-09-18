0273730b5f69aaf22f8afcaea7ca46b1664c8678
ma-che-marie <143934357+ma-che-marie@users.noreply.github.com>
2023-09-18 14:33:45 -0700

    Update 2023-09-03.js

--- src/js/data/2023-09-03.js
+++ src/js/data/2023-09-03.js
@@ -74,7 +74,7 @@ dataSet[dataSetVersion].characterData = [
     name: "이벼리 | Lee Byeori",
     img: "PSAS LEFT.png",
     opts: {
-     season: ["S1"],
+      season: ["S1"],
       type: ["classical"],
       range: ["tenor"]
     },
@@ -83,7 +83,7 @@ dataSet[dataSetVersion].characterData = [
     name: "백인태 | Paek Intae",
     img: "PSAS LEFT.png",
     opts: {
-season: ["S1"],
+      season: ["S1"],
       type: ["classical"],
       range: ["tenor"]    
     }
@@ -92,7 +92,7 @@ season: ["S1"],
     name: "유슬기 | Yoo Seulgi",
     img: "PSAS LEFT.png",
     opts: {
-season: ["S1"],
+      season: ["S1"],
       type: ["classical"],
       range: ["tenor"]    
     }
@@ -110,7 +110,7 @@ season: ["S1"],
     name: "박상돈 | Park Sangdon",
     img: "PSAS LEFT.png",
     opts: {
-season: ["S1"],
+      season: ["S1"],
       type: ["classical"],
       range: ["baritone"]      },
   },
@@ -118,7 +118,7 @@ season: ["S1"],
     name: "권서겅 | Kwon Seokyung",
     img: "PSAS LEFT.jpg",
     opts: {
- season: ["S1"],
+      season: ["S1"],
       type: ["classical"],
       range: ["bb"]    }
   },
@@ -126,7 +126,7 @@ season: ["S1"],
     name: "이동신 | Lee Dongshin",
     img: "PSAS LEFT.jpg",
     opts: {
-season: ["S1"],
+      season: ["S1"],
       type: ["classical"],
       range: ["tenor"] 
     }
@@ -135,7 +135,7 @@ season: ["S1"],
     name: "백형훈 | Baek Hyeonghun",
     img: "PSAS LEFT.jpg",
     opts: {
-season: ["S1"],
+      season: ["S1"],
       type: ["musical"],
       range: ["tenor"]   
     }
@@ -144,373 +144,477 @@ season: ["S1"],
     name: "고은성 | Ko Eunsung",
     img: "PSAS LEFT.jpg",
     opts: {
-season: ["S1"],
+      season: ["S1"],
       type: ["musical"],
       range: ["tenor"]   
 },
   },
 	{
-    name: "Eriko Nakamura",
+    name: "조민규 | Cho Mingyu",
     img: "PSAS LEFT.jpg",
     opts: {
-      season: ["S1"]
+      season: ["S2"],
+      type: ["classical"],
+      range: ["tenor"]     
     }
   },
   {
-    name: "Asami Imai",
+    name: "배두훈 | Bae Doohoon",
     img: "PSAS LEFT.jpg",
     opts: {
-      season: ["S1"]
+      season: ["S2"],
+      type: ["musical"],
+      range: ["tenor"]   
     }
   },
   {
-    name: "Yurina Hase",
+    name: "강형호 | Kang Hyungho",
     img: "PSAS LEFT.jpg",
     opts: {
-      season: ["S1"]
+      season: ["S2"],
+      type: ["rockpop"],
+      range: ["tenor", "counter"]   
     }
   },
   {
-    name: "Azumi Asakura",
+    name: "고우림 | Ko Woorim",
     img: "PSAS LEFT.jpg",
     opts: {
-      season: ["S1"]
+      season: ["S2"],
+      type: ["classical"],
+      range: ["bass"]     
     },
   },
 	{
-    name: "Eriko Nakamura",
+    name: "김주택 | Kim Jootaek",
     img: "PSAS LEFT.jpg",
     opts: {
-      season: ["S1"]
+      season: ["S2"],
+      type: ["classical"],
+      range: ["baritone"]    
     }
   },
   {
-    name: "Asami Imai",
+    name: "정필립 | Jeong Phillip",
     img: "PSAS LEFT.jpg",
     opts: {
-      season: ["S1"]
+      season: ["S2"],
+      type: ["classical"],
+      range: ["tenor"]    
     }
   },
   {
-    name: "Yurina Hase",
+    name: "박강현 | Park Kanghyun",
     img: "PSAS LEFT.jpg",
     opts: {
-      season: ["S1"]
+      season: ["S2"],
+      type: ["musical"],
+      range: ["tenor"]    
     }
   },
   {
-    name: "Azumi Asakura",
+    name: "한태인 | Han Taein",
     img: "PSAS LEFT.jpg",
     opts: {
-      season: ["S1"]
+      season: ["S2"],
+      type: ["classical"],
+      range: ["bass"]    
     },
   },
 	{
-    name: "Eriko Nakamura",
+    name: "조형균 | Zo Hyunggyun",
     img: "PSAS LEFT.jpg",
     opts: {
-      season: ["S1"]
+      season: ["S2"],
+      type: ["musical"],
+      range: ["baritone"]    
     }
   },
   {
-    name: "Asami Imai",
+    name: "이충주 | Lee Choongjoo",
     img: "PSAS LEFT.jpg",
     opts: {
-      season: ["S1"]
+      season: ["S2"],
+      type: ["classical", "musical"],
+      range: ["baritone"]    
     }
   },
   {
-    name: "Yurina Hase",
+    name: "김동현 | Kim Donghyun",
     img: "PSAS LEFT.jpg",
     opts: {
-      season: ["S1"]
+      season: ["S2"],
+      type: ["classical"],
+      range: ["bb"]    
     }
   },
   {
-    name: "Azumi24 Asakura",
+    name: "안세권 | Ahn Sekwon",
     img: "PSAS LEFT.jpg",
     opts: {
-      season: ["S1"]
+      season: ["S2"],
+      type: ["classical"],
+      range: ["tenor"]    
     },
   },
 	{
-    name: "Eriko Nakamura",
+    name: "유채훈 | You Chaehoon",
     img: "PSAS LEFT.jpg",
     opts: {
-      season: ["S1"]
+      season: ["S3"],
+      type: ["classical"],
+      range: ["tenor"]      
     }
   },
   {
-    name: "Asami Imai",
+    name: "최성훈 | Choi Sunghoon",
     img: "PSAS LEFT.jpg",
     opts: {
-      season: ["S1"]
+      season: ["S3"],
+      type: ["classical"],
+      range: ["counter"]    
     }
   },
   {
-    name: "Yurina Hase",
+    name: "정민성 | Jeong Minseong",
     img: "PSAS LEFT.jpg",
     opts: {
-      season: ["S1"]
+      season: ["S3"],
+      type: ["classical"],
+      range: ["baritone"]    
     }
   },
   {
-    name: "Azumi Asakura",
+    name: "박기훈 | Park Kihun",
     img: "PSAS LEFT.jpg",
     opts: {
-      season: ["S1"]
+      season: ["S3"],
+      type: ["classical"],
+      range: ["tenor"]    
     },
   },
 	{
-    name: "Eriko Nakamura",
+    name: "김바울 | Kim Paul",
     img: "PSAS LEFT.jpg",
     opts: {
-      season: ["S1"]
+      season: ["S3"],
+      type: ["classical"],
+      range: ["bass"]    
     }
   },
   {
-    name: "Asami Imai",
+    name: "존노 | John Noh",
     img: "PSAS LEFT.jpg",
     opts: {
-      season: ["S1"]
+      season: ["S3"],
+      type: ["classical"],
+      range: ["tenor"]    
     }
   },
   {
-    name: "Yurina Hase",
+    name: "고영열 | Ko Yeongyeol",
     img: "PSAS LEFT.jpg",
     opts: {
-      season: ["S1"]
+      season: ["S3"],
+      type: ["gugak"],
+      range: ["tenor"]    
     }
   },
   {
-    name: "Azumi Asakura",
+    name: "황건하 | Hwang Gunha",
     img: "PSAS LEFT.jpg",
     opts: {
-      season: ["S1"]
+      season: ["S3"],
+      type: ["musical"],
+      range: ["baritone"]    
     },
   },
 	{
-    name: "Eriko Nakamura",
+    name: "길병민 | Gil Byeongmin",
     img: "PSAS LEFT.jpg",
     opts: {
-      season: ["S1"]
+      season: ["S3"],
+      type: ["classical"],
+      range: ["bb"]    
     }
   },
   {
-    name: "Asami Imai",
+    name: "김성식 | Kim Sungsik",
     img: "PSAS LEFT.jpg",
     opts: {
-      season: ["S1"]
+      season: ["S3"],
+      type: ["musical"],
+      range: ["baritone"]    
     }
   },
   {
-    name: "Yurina Hase",
+    name: "김민석 | Kim Minseok",
     img: "PSAS LEFT.jpg",
     opts: {
-      season: ["S1"]
+      season: ["S3"],
+      type: ["classical"],
+      range: ["tenor"]    
     }
   },
   {
-    name: "Azumi 36Asakura",
+    name: "박현수 | Park Hyunsoo",
     img: "PSAS LEFT.jpg",
     opts: {
-      season: ["S1"]
+      season: ["S3"],
+      type: ["classical"],
+      range: ["baritone"]    
     },
   },
 	{
-    name: "Eriko Nakamura",
+    name: "김지훈 | Kim Jihoon",
     img: "PSAS LEFT.jpg",
     opts: {
-      season: ["S1"]
+      season: ["S4"],
+      type: ["classical", "musical"],
+      range: ["baritone"]    
     }
   },
   {
-    name: "Asami Imai",
+    name: "진원 | Jin Won",
     img: "PSAS LEFT.jpg",
     opts: {
-      season: ["S1"]
+      season: ["S4"],
+      type: ["classical"],
+      range: ["tenor"]    
     }
   },
   {
-    name: "Yurina Hase",
+    name: "정승원 | Jung Seungwon",
     img: "PSAS LEFT.jpg",
     opts: {
-      season: ["S1"]
+      season: ["S4"],
+      type: ["classical"],
+      range: ["tenor"]    
     }
   },
   {
-    name: "Azumi Asakura",
+    name: "노현우 | Roh Hyunwoo",
     img: "PSAS LEFT.jpg",
     opts: {
-      season: ["S1"]
+      season: ["S4"],
+      type: ["classical"],
+      range: ["baritone"]    
     },
   },
 	{
-    name: "Eriko Nakamura",
+    name: "오스틴킴 | Austin Kim",
     img: "PSAS LEFT.jpg",
     opts: {
-      season: ["S1"]
+      season: ["S4"],
+      type: ["classical"],
+      range: ["counter"]    
     }
   },
   {
-    name: "Asami Imai",
+    name: "이동규 | Lee Dongqyu",
     img: "PSAS LEFT.jpg",
     opts: {
-      season: ["S1"]
+      season: ["S4"],
+      type: ["classical"],
+      range: ["counter"]    
     }
   },
   {
-    name: "Yurina Hase",
+    name: "서영택 | Seo Yeongtaek",
     img: "PSAS LEFT.jpg",
     opts: {
-      season: ["S1"]
+      season: ["S4"],
+      type: ["classical"],
+      range: ["tenor"]    
     }
   },
   {
-    name: "Azumi Asakura",
+    name: "김성현 | Kim Seonghyeon",
     img: "PSAS LEFT.jpg",
     opts: {
-      season: ["S1"]
+      season: ["S4"],
+      type: ["classical"],
+      range: ["tenor"]    
     },
   },
 	{
-    name: "Eriko Nakamura",
+    name: "임규형 | Lim Kyuhyung",
     img: "PSAS LEFT.jpg",
     opts: {
-      season: ["S1"]
+      season: ["S4"],
+      type: ["musical"],
+      range: ["tenor"]    
     }
   },
   {
-    name: "Asami Imai",
+    name: "조진호 | Jo Jinho",
     img: "PSAS LEFT.jpg",
     opts: {
-      season: ["S1"]
+      season: ["S4"],
+      type: ["rockpop"],
+      range: ["tenor"]    
     }
   },
   {
-    name: "Yurina Hase",
+    name: "김수인 | Kim Suin",
     img: "PSAS LEFT.jpg",
     opts: {
-      season: ["S1"]
+      season: ["S4"],
+      type: ["gugak"],
+      range: ["tenor"]    
     }
   },
   {
-    name: "Azumi 48Asakura",
+    name: "이승민 | Lee Seungmin",
     img: "PSAS LEFT.jpg",
     opts: {
-      season: ["S1"]
+      season: ["S4"],
+      type: ["classical"],
+      range: ["baritone"]    
     },
   },
 	{
-    name: "Eriko Nakamura",
+    name: "박준범 | Park Junbeom",
     img: "PSAS LEFT.jpg",
     opts: {
-      season: ["S1"]
+      season: ["S4all"],
+      type: ["classical"],
+      range: ["baritone"]    
     }
   },
   {
-    name: "Asami Imai",
+    name: "김우성 | Kim Wooseong",
     img: "PSAS LEFT.jpg",
     opts: {
-      season: ["S1"]
+      season: ["S4all"],
+      type: ["musical"],
+      range: ["baritone"]    
     }
   },
   {
-    name: "Yurina Hase",
+    name: "이해준 | Lee Haejun",
     img: "PSAS LEFT.jpg",
     opts: {
-      season: ["S1"]
+      season: ["S4all"],
+      type: ["musical"],
+      range: ["tenor"]    
     }
   },
   {
-    name: "Azumi Asakura",
+    name: "이기현 | Lee Kihyun",
     img: "PSAS LEFT.jpg",
     opts: {
-      season: ["S1"]
+      season: ["S4all"],
+      type: ["classical"],
+      range: ["bass"]    
     },
   },
 	{
-    name: "Eriko Nakamura",
+    name: "김광진 | Kim Kwangjin",
     img: "PSAS LEFT.jpg",
     opts: {
-      season: ["S1"]
+      season: ["S4all"],
+      type: ["rockpop"],
+      range: ["tenor"]    
     }
   },
   {
-    name: "Asami Imai",
+    name: "림팍 | Rim Park",
     img: "PSAS LEFT.jpg",
     opts: {
-      season: ["S1"]
+      season: ["S4all"],
+      type: ["classical"],
+      range: ["tenor"]    
     }
   },
   {
-    name: "Yurina Hase",
+    name: "신은총 | Shin Eunchong",
     img: "PSAS LEFT.jpg",
     opts: {
-      season: ["S1"]
+      season: ["S4all"],
+      type: ["musical"],
+      range: ["tenor"]    
     }
   },
   {
-    name: "Azumi Asakura",
+    name: "안민수 | Ahn Minsoo",
     img: "PSAS LEFT.jpg",
     opts: {
-      season: ["S1"]
+      season: ["S4all"],
+      type: ["classical"],
+      range: ["bb"]    
     },
   },
 	{
-    name: "Eriko Nakamura",
+    name: "이한범 | Lee Hanbeom",
     img: "PSAS LEFT.jpg",
     opts: {
-      season: ["S1"]
+      season: ["S4all"],
+      type: ["classical"],
+      range: ["baritone"]    
     }
   },
   {
-    name: "Asami Imai",
+    name: "안혜찬 | Ahn Hyechan",
     img: "PSAS LEFT.jpg",
     opts: {
-      season: ["S1"]
+      season: ["S4all"],
+      type: ["classical"],
+      range: ["tenor"]    
     }
   },
   {
-    name: "Yurina Hase",
+    name: "김모세 | Kim Mose",
     img: "PSAS LEFT.jpg",
     opts: {
-      season: ["S1"]
+      season: ["S4all"],
+      type: ["classical"],
+      range: ["tenor"]    
     }
   },
   {
-    name: "Azumi Asakura",
+    name: "홍준기 | Hong Jungi",
     img: "PSAS LEFT.jpg",
     opts: {
-      season: ["S1"]
+      season: ["S4all"],
+      type: ["musical"],
+      range: ["tenor"]    
     },
   },
 	{
-    name: "Eriko Nakamura",
+    name: "이홍석 | Lee Hongseok",
     img: "PSAS LEFT.jpg",
     opts: {
-      season: ["S1"]
+      season: ["S4all"],
+      type: ["classical"],
+      range: ["baritone"]    
     }
   },
   {
-    name: "Asami Imai",
+    name: "이세헌 | Lee Seheon",
     img: "PSAS LEFT.jpg",
     opts: {
-      season: ["S1"]
+      season: ["S4all"],
+      type: ["musical"],
+      range: ["tenor"]    
     }
   },
   {
-    name: "Yurina Hase",
+    name: "윤현선 | Yun Hyunsun",
     img: "PSAS LEFT.jpg",
     opts: {
-      season: ["S1"]
+      season: ["S4all"],
+      type: ["musical"],
+      range: ["tenor"]    
     }
   },
   {
-    name: "Azumi Asakura",
+    name: "임현준 | Lim Hyunjoon",
     img: "PSAS LEFT.jpg",
     opts: {
-      season: ["S1"]
+      season: ["S4all"],
+      type: ["classical"],
+      range: ["bass"]    
     },
   },
 
